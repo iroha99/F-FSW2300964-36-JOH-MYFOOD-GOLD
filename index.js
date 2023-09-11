aa40e6639fe31578a1ee0394f687f044c3d05ac1
johan <johanirving99@gmail.com>
2023-09-11 14:33:59 +0700

    Moving all routing to router

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
 const express = require('express')
 const app = express()
-const db = require('./db/db')
-const foodModel = require('./models/foodModel')
-const foodController = require('./controllers/foodController')
 
 app.set('view engine','ejs')
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: false}))
 
 const router = require('./routers/router')
 app.use(router)
-
-
-app.get('/', async (req,res) => {
-    const data = await foodModel.getAllFood();
-    res.render('./pages/index', {
-        data:data
-    })
-})
-
-app.get('/add', (req,res) => {
-    res.render('./pages/add')
-})
 
 app.listen(4000, () => 
 {
--- routers/router.js
+++ routers/router.js
@@ -1,4 +1,5 @@
 const foodController = require('../controllers/foodController')
+const foodModel = require('../models/foodModel')
 const router = require('express').Router()
 
 router.get('/api/foods', foodController.index)
@@ -6,6 +7,17 @@ router.get('/api/foods/:id', foodController.show)
 router.post('/api/foods', foodController.create)
 router.put('/api/foods/:id', foodController.update)
 router.delete('/api/foods/:id', foodController.delete)
+
+router.get('/', async (req,res) => {
+    const data = await foodModel.getAllFood();
+    res.render('./pages/index', {
+        data:data
+    })
+})
+
+router.get('/add', (req,res) => {
+    res.render('./pages/add')
+})
 
 
 module.exports = router
